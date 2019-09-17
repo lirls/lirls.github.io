aa3a546174bc3b06f5c64c162dd2f8064381d9cc
lirls <m13760375724_1@163.com>
2019-09-18 06:20:54 +0800

    Site updated: 2019-09-18 06:20:49

--- live2d-widget-master/autoload.js
+++ live2d-widget-master/autoload.js
@@ -21,7 +21,7 @@ $.ajax({
 
 //初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+	initWidget("/live2d-widget/" + "waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
 //initWidget第一个参数为waifu-tips.json的路径
 //第二个参数为api地址（无需修改）
